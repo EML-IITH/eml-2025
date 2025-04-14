1ab7671c45851053cdfad28bbbd54c94ff378ed5
ManasVasana <manasvasana@gmail.com>
2025-04-14 21:34:32 +0530

    Changed Title

---
+++ src/app/head.jsx
@@ -0,0 +1,16 @@
+
+function Head() {
+    return (
+      <>
+        <title>EML IITH</title>
+        <meta
+          name="description"
+          content="Extra Mural Lectures (EML) of IIT Hyderabad brings together visionary leaders, pioneers, and changemakers to inspire the student community through thought-provoking talks and personal journeys."
+        />
+        <link rel="icon" href="/eml1.png" />
+      </>
+    );
+}
+
+export default Head;
+  
